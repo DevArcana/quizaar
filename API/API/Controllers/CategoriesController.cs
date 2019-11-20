718da6d139f980d88a0f7ff0f4f5da0f0e28dc12
Neferius Leviathanis <neferius.leviathanis@gmail.com>
2019-11-20 13:55:07 +0100

    add category controller

---
+++ API/API/Controllers/CategoriesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Database;
+using API.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        protected readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _context.Categories;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public Category Get(long id)
+        {
+            return _context.Categories.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]Category category)
+        {
+            var cat = new Category
+            {
+                Name = category.Name
+            };
+
+            _context.Categories.Add(cat);
+            _context.SaveChanges();
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(long id, [FromBody]Category category)
+        {
+            var cat = _context.Categories.Where(x => x.Id == id).FirstOrDefault();
+
+            if (category != null)
+            {
+                category.Name = category.Name;
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+            }
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(long id)
+        {
+            var category = _context.Categories.Where(x => x.Id == id).FirstOrDefault();
+
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
